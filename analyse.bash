b9da16234fa2d4896b08af2ad5198cd961340050
Shota Napetvaridze <napetvaridze.shota@gmail.com>
2023-02-17 11:30:32 +0100

    Added a function check-commands

--- analyse.bash
+++ analyse.bash
@@ -6,7 +6,7 @@ if [ $# -eq 0 ]; then
   exit 1
 fi
 
-wget -O pic.png $1 
+wget -O pic.png "$1"
 
 # Download the resource
 if wget "$1" -q -O resource.bin; then
--- cli.bash
+++ cli.bash
@@ -17,132 +17,117 @@ VERSION="1.0.0"
 #
 # Message to display for usage and help.
 #
-function usage
-{
-    local txt=(
-"Utility $SCRIPT for doing stuff."
-"Usage: $SCRIPT [options] <command> [arguments]"
-""
-"Command:"
-"  milk             Present a daily quote and print it out nicely"
-"  moose [anything]  Present a online daily quote and print it out nicely"
-""
-"Options:"
-"  --help, -h     Print help."
-"  --version, -h  Print version."
-    )
+function usage {
+  local txt=(
+    "Utility $SCRIPT for doing stuff."
+    "Usage: $SCRIPT [options] <command> [arguments]"
+    ""
+    "Command:"
+    "  milk             Present a daily quote and print it out nicely"
+    "  moose [anything]  Present a online daily quote and print it out nicely"
+    ""
+    "Options:"
+    "  --help, -h     Print help."
+    "  --version, -v  Print version."
+  )
 
-    printf "%s\\n" "${txt[@]}"
+  printf '%s\n' "${txt[@]}"
 }
 
 #
 # Message to display when bad usage.
 #
-function badUsage
-{
-    local message="$1"
-    local txt=(
-"For an overview of the command, execute:"
-"$SCRIPT --help"
-    )
+function badUsage {
+  local message="$1"
+  local txt=(
+    "For an overview of the command, execute:"
+    "$SCRIPT --help"
+  )
 
-    [[ -n $message ]] && printf "%s\\n" "$message"
+  [[ -n $message ]] && printf '%s\n' "$message"
 
-    printf "%s\\n" "${txt[@]}"
+  printf '%s\n' "${txt[@]}"
 }
 
 #
 # Message to display for version.
 #
-function version
-{
-    local txt=(
-"$SCRIPT version $VERSION"
-    )
+function version {
+  local txt=(
+    "$SCRIPT version $VERSION"
+  )
 
-    printf "%s\\n" "${txt[@]}"
+  printf '%s\n' "${txt[@]}"
 }
 
 #
 # Function that prints daily quote.
 #
-function app-milk
-{
-    quote=$(fortune)
-    cowsay -f milk.cow $quote
+function app-milk {
+  quote=$(fortune)
+  cowsay -f milk.cow "$quote"
 }
 
 #
 # Function that uses online quote service 
-# to do get a daily quote.
+# to get a daily quote.
 #
-function app-moose
-{
-    quote=$(curl -s "https://api.quotable.io/random" | jq '.content' | sed 's/^"\(.*\)"$/\1/')
-    cowsay -f moose.cow $quote
+function app-moose {
+  quote=$(curl -s "https://api.quotable.io/random" | jq -r '.content')
+  cowsay -f moose.cow "$quote"
 }
 
+#
+# Check if required commands are installed
+#
+function check-commands {
+  local commands=(jq cowsay fortune)
+  local os=$(uname)
+
+  for cmd in "${commands[@]}"; do
+    if ! command -v "$cmd" &> /dev/null; then
+      case "$os" in
+        Linux)
+          apt-get install -y "$cmd"
+          ;;
+        Darwin)
+          brew install "$cmd"
+          ;;
+        *)
+          echo "Unknown operating system: $os"
+          exit 1
+          ;;
+      esac
+    fi
+  done
+}
+
+check-commands
 
 #
 # Process options
 #
-while (( $# ))
-# To detect the OS if jq command is not installed
-os=$(uname)
-
-if [[ "$os" == "Linux" ]]; then
-    if ! command -v jq &> /dev/null; then
-        apt-get install jq -y
-    fi 
-    if ! command -v cowsay &> /dev/null; then
-        apt install cowsay -y
-    fi 
-    if ! command -v fortune &> /dev/null; then
-        apt install fortune-mod -y
-    fi 
-elif [[ "$os" == "Darwin" ]]; then
-    if ! command -v jq &> /dev/null; then
-        brew install jq
-    fi
-    if ! command -v cowsay &> /dev/null; then
-        brew install cowsay
-    fi 
-    if ! command -v fortune &> /dev/null; then
-        brew install fortune
-    fi
-else
-    echo "Unknown operating system: $os"
-fi
-
-
-
-
-do
-    case "$1" in
-
-        --help | -h)
-            usage
-            exit 0
-        ;;
-
-        --version | -v)
-            version
-            exit 0
-        ;;
-
-        milk | moose)
-            command=$1
-            shift
-            app-"$command" "$*"
-            exit 0
-        ;;
-
-        *)
-            badUsage "Option/command not recognized."
-            exit 1
-        ;;
-
-    esac
+while (( $# )); do
+  case "$1" in
+    --help | -h)
+      usage
+      exit 0
+      ;;
+    --version | -v)
+      version
+      exit 0
+      ;;
+    milk | moose)
+      command=$1
+      shift
+      app-"$command" "$*"
+      exit 0
+      ;;
+    *)
+      badUsage "Option/command not recognized."
+      exit 1
+      ;;
+  esac
 done
 
 badUsage
